ec54a5a8e69cf1ab12abd7a80ef8cb84414ef164
Levent GUZELOGLU <gun.guzeloglu@gmail.com>
2018-10-05 16:01:37 +0300

    first commit

---
+++ src/public/js/main.js
@@ -0,0 +1,23 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+//const { createStore } = require("redux");
+//const { Provider } = require("react-redux");
+//const reducers = require("./reducers");
+
+// const App = require("./components/app/AppContainer");
+
+// const store = createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// ReactDom.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+const App = () => <h1>Hellow World</h1>;
+
+ReactDom.render(<App />, document.getElementById("root"));
